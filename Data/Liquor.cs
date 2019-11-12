e98a529a8c2cec427ce0758d96d91075e6ac20a3
Oliver <kicks.oliver@gmail.com>
2019-11-11 19:34:19 -0500

    Bulit out tables and enabled migrations

---
+++ Data/Liquor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public enum LiquorCategory
+    {
+        Irish,
+        Scotch,
+        Japanese,
+        Canadian,
+        Bourbon,
+        Tennessee,
+        Rye,
+        Blended,
+        SingleMalt
+    }
+
+    public class Liquor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public LiquorCategory Category { get; set; }
+
+        [Required]
+        public float PercentAlcohol { get; set; }
+
+        [Required]
+        public string Origin { get; set; }
+
+        [Required]
+        public double AverageRating { get; set; }
+    }
+}
---
+++ Data/RecipeRating.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class RecipeRating
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Recipe))]
+        public int RecipeId { get; set; }
+
+        public virtual Recipe Recipe { get; set; }
+
+        [Required]
+        public Guid Rater { get; set; }
+
+        [Required]
+        public double Rating { get; set; }
+    }
+}
