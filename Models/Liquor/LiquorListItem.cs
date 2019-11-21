860731a458f43ab33ef2af4aaf3edc2d99f44f46
fariaah13 <fariaah13@gmail.com>
2019-11-21 18:50:10 -0500

    Liquor Models

---
+++ Models/Liquor/LiquorListItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Liquor
+{
+    public class LiquorListItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DisplayName ("Rating")]
+        public double AverageRating { get; set; }
+    }
+}
