37036574697e288f8aa587bbd4b1d0d9162d2b50
fariaah13 <fariaah13@gmail.com>
2019-11-21 20:18:28 -0500

    migration

---
+++ Data/Migrations/201911220115170_Swagger.cs
@@ -0,0 +1,18 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Swagger : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Liquors", "AverageRating");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Liquors", "AverageRating", c => c.Double(nullable: false));
+        }
+    }
+}
